fee46008a9c3201a1e6255b152637dc2bb204d3f
Amanda Liu <aliu0131@gmail.com>
2018-08-07 17:22:17 -0400

    Cleaned up code a bit
    Here we take our first steps into something a little more generic, though it is cleaner and less of a hassle to write out.
    Gotta love for loops, amirite?
    To be fair, it is better this way - and the map still displays these school locations more or less so I'm not complaining too much.

--- map.py
+++ map.py
@@ -1,10 +1,12 @@
 import folium
 
 wmap = folium.Map(location=[43.832248, -79.309740], zoom_start=10, tiles = "Mapbox Bright")
-wmap.add_child(folium.Marker(location=[43.835193, -79.307205], popup = "Amarillo Campus", 
+
+fg = folium.FeatureGroup(name = "My Map")
+
+for coordinates in [[43.835193, -79.307205], [43.832248, -79.309740], [43.828240, -79.308264]]:
+    fg.add_child(folium.Marker(location=coordinates, popup = "Campus Location", 
                              icon = folium.Icon(color = "blue")))
-wmap.add_child(folium.Marker(location=[43.832248, -79.309740], popup = "Main Campus", 
-                        icon = folium.Icon(color = "purple")))
-wmap.add_child(folium.Marker(location = [43.828240, -79.308264], popup = "Milliken Campus", 
-                             icon = folium.Icon(color = "red")))
+
+wmap.add_child(fg)
 wmap.save("Map1.html")
